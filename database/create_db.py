1c67e609b9850906e7908d108e30808da48ff9ec
f-andrei <andreifrancap@gmail.com>
2024-01-21 20:25:49 -0300

    add: repeat days table

--- database/create_db.py
+++ database/create_db.py
@@ -7,18 +7,22 @@ DB_NAME = 'studytime.sqlite3'
 DB_FILE = ROOT_DIR / DB_DIR / DB_NAME
 
 TASKS = 'tasks'
+REPEAT_DAYS = 'repeat_days'
 
 conn = sqlite3.connect(DB_FILE)
 cur = conn.cursor()
 
-# Clear existing data from tables
-cur.execute(f'DELETE FROM {TASKS}')
-conn.commit()
 
-# Clear all ids
-cur.execute('VACUUM')
-conn.commit()
+try:
+    # Clear existing data from tables
+    cur.execute(f'DELETE FROM {TASKS}')
+    conn.commit()
 
+    # Clear all ids
+    cur.execute('VACUUM')
+    conn.commit()
+except Exception as e:
+    print(f"Error: {e}")
 
 # Create parent table
 cur.execute(
@@ -30,6 +34,16 @@ cur.execute(
     'start_date TEXT, '
     'duration FLOAT, '
     'is_repeatable INTEGER'
+    ')'
+)
+
+cur.execute(
+    f'CREATE TABLE IF NOT EXISTS {REPEAT_DAYS}'
+    '('
+    'task_id INTEGER, '
+    'day_number INTEGER, '
+    'PRIMARY KEY (task_id, day_number), '
+    f'FOREIGN KEY (task_id) REFERENCES {REPEAT_DAYS} (task_id)'
     ')'
 )
 conn.commit()

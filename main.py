03decf39a14f4d7cef551b3395e0c6bfeb0ff37e
f-andrei <andreifrancap@gmail.com>
2024-01-24 22:20:47 -0300

    add/fix: update_task, get_tasks, remind_tasks logic

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from pytz import timezone
 from tasks import Task
 from pathlib import Path
 from utils import new_task_filter
-from database.db_operations import get_task_by_id, save_repeat_days_to_database, get_last_task
+from database.db_operations import get_task_by_id, save_repeat_days_to_database, get_all_tasks, get_due_tasks
+from time import sleep
+from reminder import TaskScheduler
+
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -27,6 +30,7 @@ intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
 
+task_scheduler = TaskScheduler()
 
 @bot.event
 async def on_ready():
@@ -36,7 +40,7 @@ async def on_ready():
 		servers_count = servers_count + 1
 
 	print("StudyTime bot is in " + str(servers_count) + " servers.")
-	check_tasks.start()
+	remind_tasks.start()
 	statusloop.start()
 	
 
@@ -46,18 +50,18 @@ async def on_message(message):
 		print(message.content)
 		await message.channel.send(f"hey {message.author.mention}")
 
-	if message.content == '.tasks':
-		await message.channel.send(f"Hey {message.author.mention}, here are your tasks:")
-		await check_tasks()
-
 	await bot.process_commands(message)
 
 
 @bot.command()
 async def create_task(ctx):
 	try:
+		current_time = datetime.now()
+		five_minutes_later = current_time + timedelta(minutes=5)
+		formatted_datetime = five_minutes_later.strftime("%Y-%m-%dT%H:%M:%S")
 		await ctx.send(embed=discord.Embed(
 			title="Create Task",
+			colour=discord.Color.yellow(),
 			description="**Required Fields**\n"
 						"***Name***\n```Study```\n"
 						"***Description***\n```Study Python```\n"
@@ -65,7 +69,7 @@ async def create_task(ctx):
 						"***Duration (minutes)***\n```30```\n"
 						"***Is Repeatable (1 or 0) for True or False***\n```1```\n"
 						"***Example Usage***\n"
-						"```Study, Study Python, 2024-01-21T21:00:00, 30, 1```\n"
+						f"```Study, Study Python, {formatted_datetime}, 30, 1```\n"
 						"*Ensure that the string above is passed to the program as "
 						"a single, continuous sequence with each value separated by commas.*"))
 		await ctx.send("Waiting for your message containing the required fields...")
@@ -86,17 +90,58 @@ async def create_task(ctx):
 			await ctx.send('Task created sucessfully.')
 		return
 	except Exception as e:
-		await ctx.send(f"An error occurred: {e}")
+		print(f"Error in create_task(): {e}")
 
 
 @bot.command()
-async def update_task(ctx, task_id: int, *, new_task):
+async def update_task(ctx):
 	try:
-		task_data = get_task_by_id(task_id)
+		channel_id = 1198117804130435092
+		channel = bot.get_channel(channel_id)
+		tasks = get_all_tasks()
+		if not tasks:
+			await channel.send("No tasks found.")
+		notify_tasks = []
+		await channel.send("These are your active tasks: ")
+
+		for task in tasks:
+			task_data = {
+				"Task nº": f"```{task[0]}```",
+				"Name": f"```{task[1]}```",
+				"Start Date": f"```{task[3]}```",
+			}
+			notify_tasks.append(task_data)
+
+		for i, task_data in enumerate(notify_tasks):
+			embed = discord.Embed(colour=discord.Color.green(), title=f"Active task {i + 1}:")
+			for key, value in task_data.items():
+				embed.add_field(name=f"{key}", value=value, inline=False)
+			await channel.send(embed=embed)
+			sleep(0.5)
+
+		await ctx.send("Please send the task id (Task nº)")
+		while True:
+			msg = await bot.wait_for("message")
+			if (msg.author == ctx.author):
+				task_id = msg.content
+				task_id = int(task_id)
+				break
+		await channel.send("Type in the updated task info:"
+				 f"```Study, Study Python, asdasdad, 30, 1```\n"
+						"*Ensure that the string above is passed to the program as "
+						"a single, continuous sequence with each value separated by commas.*")
+
+		while True:
+			task_msg = await bot.wait_for("message")
+			if (task_msg.author == ctx.author):
+				new_task_data = task_msg.content
+				new_task_data = str(new_task_data)
+				break
+		await ctx.send(f"You typed {new_task_data}")
 		task = Task()
 		
 		if task_data:
-			filtered_task = new_task_filter(new_task)
+			filtered_task = new_task_filter(new_task_data)
 			
 			task.update_task(
 				task_id,
@@ -112,7 +157,7 @@ async def update_task(ctx, task_id: int, *, new_task):
 			await ctx.send(f'Task with ID {task_id} not found.')
 
 	except Exception as e:
-		await ctx.send(f"An error occurred: {e}")
+		print(f"Error in update_task(): {e}")
 
 
 @bot.command()
@@ -122,41 +167,76 @@ async def delete_task(ctx, *, task_id):
 		task.delete_task(task_id)
 		await ctx.send(f"Task deleted sucessfully.")
 	except Exception as e:
-		await ctx.send(f"An error occured: {e}")
+		print(f"Error in delete_task(): {e}")
 
 
-@tasks.loop(seconds=300)
-async def check_tasks():
-	sao_paulo = timezone('America/Sao_Paulo')
-	current_time_utc = datetime.utcnow()
-	current_time_sao_paulo = current_time_utc.replace(tzinfo=timezone('UTC')).astimezone(sao_paulo)
+@bot.command(name='tasks')
+async def all_tasks(ctx):
+	try:
+		channel_id = 1198117804130435092
+		channel = bot.get_channel(channel_id)
+		tasks = get_all_tasks()
+		if not tasks:
+			await channel.send("No tasks found.")
+		notify_tasks = []
+		for task in tasks:
+			task_data = {
+				"Task nº": f"```{task[0]}```",
+				"Name": f"```{task[1]}```",
+				"Description": f"```{task[2]}```",
+				"Start Date": f"```{task[3]}```",
+				"Duration": f"```{task[4]}```",
+				"Is Repeatable": f"```{task[5]}```"
+			}
+			notify_tasks.append(task_data)
 
+		for i, task_data in enumerate(notify_tasks):
+			embed = discord.Embed(colour=discord.Color.green(), title=f"Active task {i + 1}:")
+			for key, value in task_data.items():
+				embed.add_field(name=f"{key}", value=value, inline=False)
+			await channel.send(embed=embed)
+			sleep(0.5)
+	except Exception as e:
+		print(f"Error in all_tasks(): {e}")
+
+
+
+@tasks.loop(seconds=5)
+async def remind_tasks():
 	channel_id = 1198117804130435092
 	channel = bot.get_channel(channel_id)
 	print("Checking for tasks...")
-	# Connect to the database
-	with sqlite3.connect('database/studytime.sqlite3') as connection:
-		try:
-			cursor = connection.cursor()
-			cursor.execute("SELECT * FROM tasks WHERE start_date BETWEEN ? AND ?",
-				   (current_time_sao_paulo - timedelta(minutes=15), current_time_sao_paulo))
-			due_tasks = cursor.fetchall()
-			notify_tasks = []
-			for task in due_tasks:
-				task_data = f"""
-					Task nº: {task[0]}
-					Name: {task[1]}
-					Description: {task[2]}
-					Start date: {task[3]}
-					Duration: {task[4]}
-					Is repeatable: {task[5]}
-				"""
-				notify_tasks.append(task_data)
-				await channel.send(f"""Reminder:
-					Active tasks:
-					{''.join(notify_tasks)}""")
-		except Exception as e:
-			await channel.send(f"An error occurred: {e}")
+	try:
+		await task_scheduler.update_schedule()
+		due_task_ids = await task_scheduler.get_due_task_ids()
+		notify_tasks = []
+		if due_task_ids:
+			for task_id in due_task_ids:
+				try:
+					task_data = get_task_by_id(task_id)
+					task_data = task_data[0]
+					task_data = {
+						"Task nº": f"```{task_data[0]}```",
+						"Name": f"```{task_data[1]}```",
+						"Description": f"```{task_data[2]}```",
+						"Start Date": f"```{task_data[3]}```",
+						"Duration": f"```{task_data[4]}```",
+						"Is Repeatable": f"```{task_data[5]}```"
+					}
+					notify_tasks.append(task_data)
+				except IndexError:
+					due_task_ids.remove(task_id)
+					print(f"IndexError: Task with ID {task_id} not found.")
+		if notify_tasks:
+			for i, task_data in enumerate(notify_tasks):
+				embed = discord.Embed(colour=discord.Color.green(), title=f"Active task {i + 1}:")
+				for key, value in task_data.items():
+					embed.add_field(name=f"{key}", value=value, inline=False)
+				await channel.send(embed=embed)
+				await asyncio.sleep(295)
+		task_scheduler.on_off(stop='true')
+	except Exception as e:
+		print(f"Error in remind_tasks(): {e}")
 
 
 @tasks.loop(seconds=10)
@@ -225,8 +305,16 @@ class DaysToRepeatView(discord.ui.View):
 
     @discord.ui.button(label="Send", style=discord.ButtonStyle.success)
     async def send(self, interaction: discord.Interaction, button: discord.ui.Button):
+        channel_id = 1198117804130435092
+        channel = bot.get_channel(channel_id)
         save_repeat_days_to_database(self.days)
-        await interaction.response.send_message(f"Task created sucessfully.")
+        for child in self.children:
+            child.disabled = True
+
+        await interaction.response.edit_message(view=self)
+        await channel.send(f"Task created successfully.")
+
+
 
 
 bot.run(token)

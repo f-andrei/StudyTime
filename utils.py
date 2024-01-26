0d691e7b31d1ada01db8814cdcfd2035c6c2480d
f-andrei <andreifrancap@gmail.com>
2024-01-25 23:41:50 -0300

    fix: removed unused packing

--- utils.py
+++ utils.py
@@ -2,5 +2,4 @@ def new_task_filter(new_task):
     filtered_task = new_task.split(", ")
     filtered_task[3] = float(filtered_task[3]) 
     filtered_task[4] = int(filtered_task[4]) 
-    name, description, start_date, duration, is_repeatable = filtered_task
     return filtered_task

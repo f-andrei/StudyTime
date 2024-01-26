4b6beb5afa3c0a8a084b7bb5b2f0dc1f30733613
f-andrei <andreifrancap@gmail.com>
2024-01-25 23:39:59 -0300

    add: cfg

---
+++ config.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_DIR = 'database'
+DB_NAME = 'studytime.sqlite3'
+DB_FILE = os.path.join(ROOT_DIR, DB_DIR, DB_NAME)
+
+
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='.', intents=intents)

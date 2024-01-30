6c57c455251706675703a7d8e0fdaeef98c64cec
f-andrei <andreifrancap@gmail.com>
2024-01-29 21:15:40 -0300

    fix: better error handling

--- database/create_db.py
+++ database/create_db.py
@@ -9,42 +9,51 @@ DB_FILE = ROOT_DIR / DB_DIR / DB_NAME
 TASKS = 'tasks'
 REPEAT_DAYS = 'repeat_days'
 
-conn = sqlite3.connect(DB_FILE)
-cur = conn.cursor()
+
+def initialize_database():
+    try:
+        with sqlite3.connect(DB_FILE) as conn:
+            cur = conn.cursor()
+
+            # Clear existing data from tables
+            clear_tables(cur, TASKS)
+            clear_tables(cur, REPEAT_DAYS)
+
+            # Clear all ids
+            cur.execute('VACUUM')
+            conn.commit()
+
+            # Create parent table
+            create_table(cur, TASKS, [
+                ('id', 'INTEGER PRIMARY KEY AUTOINCREMENT'),
+                ('name', 'TEXT'),
+                ('description', 'TEXT'),
+                ('start_date', 'TEXT'),
+                ('duration', 'FLOAT'),
+                ('is_repeatable', 'INTEGER')
+            ])
+
+            # Create child table
+            create_table(cur, REPEAT_DAYS, [
+                ('task_id', 'INTEGER'),
+                ('day_number', 'INTEGER'),
+                ('PRIMARY KEY', '(task_id)'),
+                (f'FOREIGN KEY (task_id) REFERENCES {TASKS} (id)', '')
+            ])
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 
-try:
-    # Clear existing data from tables
-    cur.execute(f'DELETE FROM {TASKS}')
-    conn.commit()
+def clear_tables(cursor, table_name):
+    cursor.execute(f'DELETE FROM {table_name}')
 
-    # Clear all ids
-    cur.execute('VACUUM')
-    conn.commit()
-except Exception as e:
-    print(f"Error: {e}")
 
-# Create parent table
-cur.execute(
-    f'CREATE TABLE IF NOT EXISTS {TASKS}'
-    '('
-    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-    'name TEXT, '
-    'description TEXT, '
-    'start_date TEXT, '
-    'duration FLOAT, '
-    'is_repeatable INTEGER'
-    ')'
-)
+def create_table(cursor, table_name, columns):
+    columns_str = ', '.join([f"{name} {data_type}" for name, data_type in columns])
+    query = f'CREATE TABLE IF NOT EXISTS {table_name} ({columns_str})'
+    cursor.execute(query)
 
-cur.execute(
-    f'CREATE TABLE IF NOT EXISTS {REPEAT_DAYS}'
-    '('
-    'task_id INTEGER, '
-    'day_number INTEGER, '
-    'PRIMARY KEY (task_id), '
-    f'FOREIGN KEY (task_id) REFERENCES {REPEAT_DAYS} (task_id)'
-    ')'
-)
-conn.commit()
-conn.close()
+
+if __name__ == "__main__":
+    initialize_database()

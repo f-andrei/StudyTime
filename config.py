f30ddf3d0b0065d90c1f6b678e3247fe486cc173
f-andrei <andreifrancap@gmail.com>
2024-01-29 21:13:44 -0300

    add: constants

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+from pytz import timezone
 import os
 
 load_dotenv()
@@ -15,3 +17,10 @@ DB_FILE = os.path.join(ROOT_DIR, DB_DIR, DB_NAME)
 intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
+
+CHANNEL_ID = 1198117804130435092
+
+SAO_PAULO_TZ = timezone('America/Sao_Paulo')
+CURRENT_TIME_UTC = datetime.utcnow()
+SAO_PAULO_NOW = CURRENT_TIME_UTC.replace(tzinfo=timezone('UTC')).astimezone(SAO_PAULO_TZ)
+

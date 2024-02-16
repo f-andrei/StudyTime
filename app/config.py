8344466db8c4fa3fc612097b4dbe7595f68b6d45
f-andrei <andreifrancap@gmail.com>
2024-02-15 22:13:00 -0300

    fix: moved model to config

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+from langchain_openai import ChatOpenAI
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -5,15 +6,17 @@ import os
 
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+OPENAI_API_KEY = os.getenv("OPENAI_KEY")
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_DIR = 'database'
 DB_NAME = 'studytime.sqlite3'
 DB_FILE = os.path.join(ROOT_DIR, DB_DIR, DB_NAME)
-DISCORD_ID = discord.Object(id=1095558713277956226)
+DISCORD_ID = discord.Object(id=os.getenv('DISCORD_ID'))
 
 intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
 
-CHANNEL_ID = 1198117804130435092
+model = ChatOpenAI(model="gpt-3.5-turbo", api_key=OPENAI_API_KEY)

fd80180fd7921ecaf194df1abe182bebc7dda863
f-andrei <andreifrancap@gmail.com>
2024-02-21 18:01:53 -0300

    add: DELETE_AFTER (to delete messages afeter x seconds)

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_TOKEN")
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -14,9 +14,9 @@ DB_DIR = 'database'
 DB_NAME = 'studytime.sqlite3'
 DB_FILE = os.path.join(ROOT_DIR, DB_DIR, DB_NAME)
 DISCORD_ID = discord.Object(id=os.getenv('DISCORD_ID'))
-
+DELETE_AFTER = 60
 intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
 
-model = ChatOpenAI(model="gpt-3.5-turbo", api_key=OPENAI_API_KEY)
+model = ChatOpenAI(model="gpt-4-turbo-preview", api_key=OPENAI_API_KEY)

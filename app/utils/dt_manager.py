56b87b2908d323295d2b651939c64831d60bfa42
f-andrei <andreifrancap@gmail.com>
2024-02-27 01:38:40 -0300

    fix: duplicated code

--- app/utils/dt_manager.py
+++ app/utils/dt_manager.py
@@ -1,63 +1,6 @@
 from datetime import datetime, timedelta
 from pytz import timezone
 from typing import Optional, Tuple
-import sqlite3
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).parent.parent
-DB_DIR = 'database'
-DB_NAME = 'studytime.sqlite3'
-DB_FILE = ROOT_DIR / DB_DIR / DB_NAME
-TASK_TABLE = 'tasks'
-REPEAT_DAYS_TABLE = 'repeat_days'
-
-
-
-# Create a connection to the SQLite database
-def establish_connection():
-    """Establish a connection to the SQLite database."""
-    connection = sqlite3.connect(str(DB_FILE))
-    return connection
-
-def get_last_task():
-    with establish_connection() as connection:
-        try:
-            cursor = connection.cursor()
-            cursor.execute(f'SELECT * FROM {TASK_TABLE} ORDER BY id DESC LIMIT 1')
-            task = cursor.fetchone()
-            if task:
-                return task[0]
-            else:
-                return None
-        except Exception as e:
-            print(f"Error fetching task from database: {e}")
-
-
-
-def save_repeat_days_to_database(days):
-    with establish_connection() as connection:
-        try:
-            task_id = get_last_task()
-            cursor = connection.cursor()
-            for day in days:
-                cursor.execute(f"INSERT INTO {REPEAT_DAYS_TABLE} (task_id, date) VALUES (?, ?)",
-                                (task_id, day))
-            connection.commit()
-        except Exception as e:
-            print(f"Error inserting task days into the database: {e}")
-
-def save_task_to_database(name, description, links, start_date, time, duration, user_id):
-    with establish_connection() as connection:
-        try:
-            cursor = connection.cursor()
-            cursor.execute(f"INSERT INTO {TASK_TABLE} (name, description, links, start_date, time, duration, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                        (name, description, links, start_date, time, duration, user_id))
-            connection.commit()
-            
-            print(f"Task '{name}' successfully inserted into the database.")
-        except Exception as e:
-            print(f"Error inserting task into the database: {e}")
-
 
 
 class DateTimeManager:

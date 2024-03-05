4337d329744ba2a57e3c1a002c85a97f68a41f13
f-andrei <andreifrancap@gmail.com>
2024-03-05 10:51:16 -0300

    fix: modified note fields dict, improved overall readability

--- app/utils/embed_utils.py
+++ app/utils/embed_utils.py
@@ -21,54 +21,44 @@ async def display_embed(data: dict, task_id=None, title=None, type=None, del_aft
             "Time": data["time"], 
             "Duration": data["duration"], 
         }
+        number_to_day = {
+            6: "Sunday",
+            0: "Monday",
+            1: "Tuesday",
+            2: "Wednesday",
+            3: "Thursday",
+            4: "Friday",
+            5: "Saturday"
+        }
+        tasks = Tasks()
+        repeat_days = tasks.get_repeat_days(task_id)
+        if repeat_days:
+            try:
+                for i in range(len(repeat_days)):
+                    day = repeat_days[i]["day_number"]
+                    repeat_days_list.append(number_to_day[day])
 
+                is_repeatable = repeat_days_list
+                if len(is_repeatable) > 1:
+                    is_repeatable = ', '.join(is_repeatable)
+                else:
+                    is_repeatable = is_repeatable[0]
+            except Exception as e:
+                print(f"Error at display_embeds: {e}")
+                raise e
+        else:
+            is_repeatable = 'Not repeatable'
+
+        fields["Repeat days"] = is_repeatable
         
 
     if type == 'note':
-        match len(data):
-            case 6:
-                task_id, name, description, links, created_at, user_id = data
-            case 5:
-                task_id, name, description, links, created_at = data
-            case 4:
-                name, description, links, created_at = data
-            case 3:
-                name, description, links = data
-
-
-    number_to_day = {
-        6: "Sunday",
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
-        5: "Saturday"
-    }
-    tasks = Tasks()
-    repeat_days = tasks.get_repeat_days(task_id)
-    if repeat_days:
-        try:
-            for i in range(len(repeat_days)):
-                day = repeat_days[i]["day_number"]
-                repeat_days_list.append(number_to_day[day])
-
-            is_repeatable = repeat_days_list
-            if len(is_repeatable) > 1:
-                is_repeatable = ', '.join(is_repeatable)
-            else:
-                is_repeatable = is_repeatable[0]
-        except Exception as e:
-            print(f"Error at display_embeds: {e}")
-            raise e
-    else:
-        is_repeatable = 'Not repeatable'
-
-    fields["Repeat days"] = is_repeatable
-
-        
-    if type == 'note':
-        fields = {"Name": name, "Description": description, "Links": links}
+       fields = {
+           "Name": data["name"],
+           "Description": data["description"],
+           "Links": data["links"],
+           "Created at": data["created_at"],
+       }
     embed = discord.Embed(colour=color, title=title)
     for field, value in fields.items():
         if not value:
